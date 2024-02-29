a48e9859fc5a97cbf9fce2a77c52d73482217e74
victor-pagnozi <victor.pagnozi@gmail.com>
2024-02-29 09:45:58 -0300

    feat: created config and Environment file to link with .env file

---
+++ packages/core/src/config/Environments.ts
@@ -0,0 +1,7 @@
+export const Environments = {
+  SMS_API_EMAIL: process.env.SMS_API_EMAIL,
+  SMS_API_PASSWORD: process.env.SMS_API_PASSWORD,
+  SMS_API_CENTRAL_CUSTOM_ID: process.env.SMS_API_CENTRAL_CUSTOM_ID,
+  SMS_API_PRODUTO_ID: process.env.SMS_API_PRODUTO_ID,
+  SMS_API_BASE_URL: process.env.SMS_API_BASE_URL,
+};
