e4c95df2fec9adeeb7cbd96c735673892d4a2c23
Vinicius Menezes <viny2999@gmail.com>
2024-03-01 16:20:56 -0300

    some models

---
+++ packages/core/src/models/acesso/acessoView.model.ts
@@ -0,0 +1,19 @@
+import {
+  mysqlTable,
+  varbinary,
+  int,
+  mysqlEnum,
+  datetime,
+} from "drizzle-orm/mysql-core";
+import { sql } from "drizzle-orm";
+
+export const acessoView = mysqlTable("acesso_view", {
+  id_acesso: varbinary("id_acesso", { length: 36 }),
+  id_cliente: varbinary("id_cliente", { length: 36 }),
+  id_api_acesso: int("id_api_acesso"),
+  id_empresa: int("id_empresa"),
+  id_acesso_tipo: int("id_acesso_tipo"),
+  status: mysqlEnum("status", ["ativo", "inativo"]).notNull().default("ativo"),
+  created_at: datetime("created_at").default(sql`CURRENT_TIMESTAMP`),
+  updated_at: datetime("updated_at").default(sql`CURRENT_TIMESTAMP`),
+});
---
+++ packages/core/src/models/assinatura/assinaturaCliente.model.ts
@@ -0,0 +1,29 @@
+import {
+  mysqlTable,
+  int,
+  datetime,
+  varchar,
+  mysqlEnum,
+  date,
+} from "drizzle-orm/mysql-core";
+import { sql } from "drizzle-orm";
+
+export const assinaturaCliente = mysqlTable("assinatura_cliente", {
+  id_assinatura_cliente: int("id_assinatura_cliente").notNull().primaryKey(),
+  id_pedido: varchar("id_pedido", { length: 16 }),
+  id_cliente: varchar("id_cliente", { length: 16 }),
+  id_empresa: int("id_empresa"),
+  ciclo: int("ciclo"),
+  id_assinatura_status: int("id_assinatura_status").notNull(),
+  id_plano: int("id_plano"),
+  pag_order_sub: varchar("pag_order_sub", { length: 32 }),
+  recorrencia: mysqlEnum("recorrencia", ["0", "1"]).default("0"),
+  recorrencia_periodo: int("recorrencia_periodo").notNull().default(1),
+  data_inicio: date("data_inicio"),
+  data_assinatura_ate: date("data_assinatura_ate"),
+  data_proxima_cobranca: date("data_proxima_cobranca"),
+  data_ultimo_pagamento: date("data_ultimo_pagamento"),
+  data_cancelamento: date("data_cancelamento"),
+  created_at: datetime("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+  updated_at: datetime("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+});
