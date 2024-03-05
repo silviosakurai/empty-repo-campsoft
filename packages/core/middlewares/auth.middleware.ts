561f2d77c5cc32c3afc718636ef56cf05c3dd653
Maycon Douglas <contatomaycon@gmail.com>
2024-03-04 23:23:35 -0300

    feat: key api

--- packages/core/middlewares/auth.middleware.ts
+++ packages/core/middlewares/auth.middleware.ts
@@ -1,13 +1,16 @@
 import { HTTPStatusCode } from "@core/common/enums/HTTPStatusCode";
 import { sendResponse } from "@core/common/functions/sendResponse";
+import { ViewApiUseCase } from "@core/useCases/api/ViewApi.useCase";
+import { ViewApiRequest } from "@core/useCases/api/dtos/ViewApiRequest.dto";
 import { FastifyReply, FastifyRequest } from "fastify";
 import fp from "fastify-plugin";
+import { container } from "tsyringe";
 
 async function authenticate(
   request: FastifyRequest,
   reply: FastifyReply
 ): Promise<void> {
-  const { authorization } = request.headers;
+  const { authorization, keyapi } = request.headers;
   const { t } = request;
 
   if (authorization) {
@@ -19,6 +22,30 @@ async function authenticate(
       return sendResponse(reply, {
         message: t("not_authorized"),
         httpStatusCode: HTTPStatusCode.UNAUTHORIZED,
+      });
+    }
+  }
+
+  if (keyapi) {
+    const viewApiUseCase = container.resolve(ViewApiUseCase);
+
+    try {
+      const responseAuth = await viewApiUseCase.execute({
+        keyApi: keyapi,
+      } as ViewApiRequest);
+
+      if (!responseAuth) {
+        return sendResponse(reply, {
+          message: t("not_authorized"),
+          httpStatusCode: HTTPStatusCode.UNAUTHORIZED,
+        });
+      }
+
+      return;
+    } catch (error) {
+      return sendResponse(reply, {
+        message: t("internal_server_error"),
+        httpStatusCode: HTTPStatusCode.INTERNAL_SERVER_ERROR,
       });
     }
   }
--- packages/core/models/api/apiAccess.model.ts
+++ packages/core/models/api/apiAccess.model.ts
@@ -1,3 +1,4 @@
+import { ApiStatus } from "@core/common/enums/models/api";
 import {
   mysqlTable,
   varbinary,
@@ -8,10 +9,10 @@ import {
 
 export const apiAccess = mysqlTable("api_acesso", {
   id_api_acesso: int("id_api_acesso").notNull().primaryKey(),
-  api_nome: varbinary("api_nome", { length: 50}),
-  api_status: mysqlEnum("api_status", ["ativo", "inativo"]),
-  api_chave: varbinary("api_chave", { length: 32}),
-  api_chave_sandbox: varbinary("api_chave_sandbox", { length: 32}),
+  api_nome: varbinary("api_nome", { length: 50 }),
+  api_status: mysqlEnum("api_status", [ApiStatus.ACTIVE, ApiStatus.INACTIVE]),
+  api_chave: varbinary("api_chave", { length: 32 }),
+  api_chave_sandbox: varbinary("api_chave_sandbox", { length: 32 }),
   id_fi_zoop_split_regra: int("id_fi_zoop_split_regra"),
   created_at: timestamp("created_at").defaultNow(),
   updated_at: timestamp("updated_at").defaultNow(),
