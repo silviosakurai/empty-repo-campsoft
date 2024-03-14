78b6977232ba98b7afa28be0a862790e696901d8
Maycon Douglas <contatomaycon@gmail.com>
2024-03-14 16:02:14 -0300

    open search

--- apps/public_api/src/controllers/product/methods/viewProduct.ts
+++ apps/public_api/src/controllers/product/methods/viewProduct.ts
@@ -13,25 +13,25 @@ export const viewProduct = async (
 ) => {
   const viewProductUseCase = container.resolve(ViewProductUseCase);
   const { t, tokenKeyData } = request;
- 
+
   try {
-      const response = await viewProductUseCase.execute(
-        tokenKeyData.company_id,
-        request.params.sku,
-      );
-        
-        if (!response) {
-            return sendResponse(reply, {
-                message: t('product_not_found'),
-                httpStatusCode: HTTPStatusCode.NOT_FOUND,
+    const response = await viewProductUseCase.execute(
+      tokenKeyData.company_id,
+      request.params.sku
+    );
+
+    if (!response) {
+      return sendResponse(reply, {
+        message: t('product_not_found'),
+        httpStatusCode: HTTPStatusCode.NOT_FOUND,
       });
     }
 
     return sendResponse(reply, {
-        data: response,
-        httpStatusCode: HTTPStatusCode.OK,
+      data: response,
+      httpStatusCode: HTTPStatusCode.OK,
     });
-} catch (error) {
+  } catch (error) {
     console.error(error);
     return sendResponse(reply, {
       message: t('internal_server_error'),
