939f18752bfd010a11ea6cc0987bddfd7e3e9973
Vinicius Menezes <viny2999@gmail.com>
2024-03-25 16:37:34 -0300

    add company validation

--- packages/core/repositories/plan/UpgradePlan.repository.ts
+++ packages/core/repositories/plan/UpgradePlan.repository.ts
@@ -44,6 +44,7 @@ export class UpgradePlanRepository {
         and(
           eq(clientSignature.id_cliente, sql`UUID_TO_BIN(${clientId})`),
           eq(clientSignature.id_assinatura_status, SignatureStatus.ACTIVE),
+          eq(clientSignature.id_empresa, companyId),
         ),
       )
       .execute();
