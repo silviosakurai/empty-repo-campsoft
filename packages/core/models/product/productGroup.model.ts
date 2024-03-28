2cd99d8c4bbe614035ef7a63a5b8e8b3b12512d3
victor-pagnozi <victor.pagnozi@gmail.com>
2024-03-28 15:58:31 -0300

    fix: fixed variable name as a database of the product group

--- packages/core/models/product/productGroup.model.ts
+++ packages/core/models/product/productGroup.model.ts
@@ -10,7 +10,7 @@ export const productGroup = mysqlTable("produto_grupo", {
   id_produto_grupo: int("id_produto_grupo").notNull().primaryKey(),
   produto_grupo: varchar("produto_grupo", { length: 50 }),
   icon: varchar("icon", { length: 150 }).notNull().default("1"),
-  qtd_produtos_selecionavies: tinyint("qtd_produtos_selecionavies")
+  qtd_produtos_selecionaveis: tinyint("qtd_produtos_selecionaveis")
     .notNull()
     .default(1),
   created_at: timestamp("created_at", { mode: "string" })
