ea38a7b2ccfaf0bd98c3c23d044bf2aeee7437c2
Maycon Douglas <contatomaycon@gmail.com>
2024-03-27 23:41:46 -0300

    feat: plan

---
+++ packages/core/validations/plan/listPlanSchema.validation.ts
@@ -0,0 +1,128 @@
+import Schema from "fluent-json-schema";
+import { Language } from "@core/common/enums/Language";
+import { TagSwagger } from "@core/common/enums/TagSwagger";
+import { Status } from "@core/common/enums/Status";
+import { PlanFieldsToOrder } from "@core/common/enums/models/plan";
+import {
+  paginationReaderSchema,
+  pagingResponseSchema,
+} from "@core/common/validations/pagination.validation";
+import { SortOrder } from "@core/common/enums/SortOrder";
+
+const PlanPriceSchema = Schema.object()
+  .prop("months", Schema.number())
+  .prop("price", Schema.number())
+  .prop("discount_value", Schema.number())
+  .prop("discount_percentage", Schema.number())
+  .prop("price_with_discount", Schema.number());
+
+const ProductResponseSchema = Schema.object()
+  .prop("product_id", Schema.string())
+  .prop("status", Schema.string().enum(Object.values(Status)))
+  .prop("name", Schema.string())
+  .prop("long_description", Schema.string())
+  .prop("short_description", Schema.string())
+  .prop("marketing_phrases", Schema.string())
+  .prop("content_provider_name", Schema.string())
+  .prop("slug", Schema.string())
+  .prop(
+    "images",
+    Schema.object()
+      .prop("main_image", Schema.string())
+      .prop("icon", Schema.string())
+      .prop("logo", Schema.string())
+      .prop("background_image", Schema.string())
+  )
+  .prop(
+    "how_to_access",
+    Schema.object()
+      .prop("desktop", Schema.string())
+      .prop("mobile", Schema.string())
+      .prop("url_web", Schema.string())
+      .prop("url_ios", Schema.string())
+      .prop("url_android", Schema.string())
+  )
+  .prop(
+    "product_type",
+    Schema.object()
+      .prop("product_type_id", Schema.number())
+      .prop("product_type_name", Schema.string())
+  )
+  .prop("created_at", Schema.string().format("date-time"))
+  .prop("updated_at", Schema.string().format("date-time"));
+
+const ProductsGroupsSchema = Schema.object()
+  .prop("product_group_id", Schema.number())
+  .prop("name", Schema.string())
+  .prop("quantity", Schema.number())
+  .prop("available_products", Schema.array().items(ProductResponseSchema));
+
+const PlanSchema = Schema.object()
+  .prop("plan_id", Schema.number())
+  .prop("status", Schema.string().enum(Object.values(Status)))
+  .prop("visible_site", Schema.boolean())
+  .prop("business_id", Schema.number())
+  .prop("plan", Schema.string())
+  .prop("image", Schema.string())
+  .prop("description", Schema.string())
+  .prop("short_description", Schema.string())
+  .prop("created_at", Schema.string().format("date-time"))
+  .prop("updated_at", Schema.string().format("date-time"))
+  .prop("prices", Schema.array().items(PlanPriceSchema))
+  .prop("products", Schema.array().items(ProductResponseSchema))
+  .prop("product_groups", Schema.array().items(ProductsGroupsSchema));
+
+const ListPlanResponseSchema = Schema.object()
+  .prop("results", Schema.array().items(PlanSchema))
+  .extend(pagingResponseSchema);
+
+export const listPlanSchema = {
+  description: "Seleciona todos os planos",
+  tags: [TagSwagger.plan],
+  produces: ["application/json"],
+  security: [
+    {
+      authenticateKeyApi: [],
+    },
+  ],
+  headers: Schema.object().prop(
+    "Accept-Language",
+    Schema.string()
+      .description("Idioma preferencial para a resposta")
+      .enum(Object.values(Language))
+      .default(Language.pt)
+  ),
+  querystring: Schema.object()
+    .prop("id", Schema.string())
+    .prop(
+      "status",
+      Schema.string().enum(Object.values(Status)).default(Status.ACTIVE)
+    )
+    .prop("plan", Schema.string())
+    .prop("description", Schema.string())
+    .prop("sort_by", Schema.string().enum(Object.keys(PlanFieldsToOrder)))
+    .prop("sort_order", Schema.string().enum(Object.values(SortOrder)))
+    .extend(paginationReaderSchema),
+  response: {
+    200: Schema.object()
+      .description("Successful")
+      .prop("status", Schema.boolean())
+      .prop("message", Schema.string())
+      .prop("data", ListPlanResponseSchema),
+    401: Schema.object()
+      .description("Unauthorized")
+      .prop("status", Schema.boolean().default(false))
+      .prop("message", Schema.string())
+      .prop("data", Schema.null()),
+    404: Schema.object()
+      .description("Not Found")
+      .prop("status", Schema.boolean().default(false))
+      .prop("message", Schema.string())
+      .prop("data", Schema.null()),
+    500: Schema.object()
+      .description("Internal Server Error")
+      .prop("status", Schema.boolean().default(false))
+      .prop("message", Schema.string())
+      .prop("data", Schema.null()),
+  },
+};
