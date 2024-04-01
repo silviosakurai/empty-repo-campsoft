d9ba8f919b726b51ac3dc009d8c733df09c82510
Maycon Douglas <contatomaycon@gmail.com>
2024-04-01 11:11:45 -0300

    i save card token

--- packages/core/interfaces/services/IZoopGateway.service.ts
+++ packages/core/interfaces/services/IZoopGateway.service.ts
@@ -1,15 +1,43 @@
-import { ResponseService } from '@core/common/interfaces/IResponseServices';
-import { ITransactionFullTicketRequest, ITransactionFullTicketResponse } from './zoop/ITransactionFullTicket';
-import { ITransactionSimpleTicketRequest, ITransactionSimpleTicketResponse } from './zoop/ITransactionSimpleTicket';
-import { ITransactionPixRequest, ITransactionPixResponse } from './zoop/ITransactionPix';
-import { ITransactionCardIdRequest, ITransactionCardRequest, ITransactionCardResponse } from './zoop/ITransactionCard';
-import { ISaveCardTokenRequest, ISaveCardTokenResponse } from './zoop/IsaveCardToken';
+import { ResponseService } from "@core/common/interfaces/IResponseServices";
+import {
+  ITransactionFullTicketRequest,
+  ITransactionFullTicketResponse,
+} from "./zoop/ITransactionFullTicket";
+import {
+  ITransactionSimpleTicketRequest,
+  ITransactionSimpleTicketResponse,
+} from "./zoop/ITransactionSimpleTicket";
+import {
+  ITransactionPixRequest,
+  ITransactionPixResponse,
+} from "./zoop/ITransactionPix";
+import {
+  ITransactionCardIdRequest,
+  ITransactionCardRequest,
+  ITransactionCardResponse,
+} from "./zoop/ITransactionCard";
+import {
+  ISaveCardTokenRequest,
+  ISaveCardTokenResponse,
+} from "./zoop/ISaveCardToken";
 
 export interface IZoopGatewayService {
-  saveCardToken: (input: ISaveCardTokenRequest) => Promise<ResponseService<ISaveCardTokenResponse>>;
-  createTransactionPix: (input: ITransactionPixRequest) => Promise<ResponseService<ITransactionPixResponse>>;
-  createTransactionFullTicket: (input: ITransactionFullTicketRequest) => Promise<ResponseService<ITransactionFullTicketResponse>>;
-  createTransactionSimpleTicket: (input: ITransactionSimpleTicketRequest) => Promise<ResponseService<ITransactionSimpleTicketResponse>>;
-  createTransactionCard: (input: ITransactionCardRequest) => Promise<ResponseService<ITransactionCardResponse>>;
-  createTransactionCardId: (input: ITransactionCardIdRequest) => Promise<ResponseService<ITransactionCardResponse>>;
+  saveCardToken: (
+    input: ISaveCardTokenRequest
+  ) => Promise<ResponseService<ISaveCardTokenResponse>>;
+  createTransactionPix: (
+    input: ITransactionPixRequest
+  ) => Promise<ResponseService<ITransactionPixResponse>>;
+  createTransactionFullTicket: (
+    input: ITransactionFullTicketRequest
+  ) => Promise<ResponseService<ITransactionFullTicketResponse>>;
+  createTransactionSimpleTicket: (
+    input: ITransactionSimpleTicketRequest
+  ) => Promise<ResponseService<ITransactionSimpleTicketResponse>>;
+  createTransactionCard: (
+    input: ITransactionCardRequest
+  ) => Promise<ResponseService<ITransactionCardResponse>>;
+  createTransactionCardId: (
+    input: ITransactionCardIdRequest
+  ) => Promise<ResponseService<ITransactionCardResponse>>;
 }
