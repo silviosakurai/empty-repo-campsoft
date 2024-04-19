082547a934e532e5c57c6e44cb4af3020ddfbd66
root <root@SILVIO-SAKURAI>
2024-04-19 16:33:17 -0300

    fix: indentation of clientbyidupdater file

--- packages/core/useCases/client/ClientByIdUpdater.useCase.ts
+++ packages/core/useCases/client/ClientByIdUpdater.useCase.ts
@@ -5,26 +5,25 @@ import { UpdateClientByIdRequestDto } from "./dtos/updateClientByIdRequest.dto";
 
 @injectable()
 export class ClientByIdUpdaterUseCase {
-    constructor(private readonly clientService: ClientService) { }
+  constructor(private readonly clientService: ClientService) {}
 
-    async update(
-        clientId: string,
-        input: UpdateClientByIdRequestDto,
-        tokenKeyData: ITokenKeyData
-    ): Promise<boolean | null> {
+  async update(
+    clientId: string,
+    input: UpdateClientByIdRequestDto,
+    tokenKeyData: ITokenKeyData
+  ): Promise<boolean | null> {
+    const userFounded = await this.clientService.view(tokenKeyData, clientId);
 
-        const userFounded = await this.clientService.view(tokenKeyData, clientId);
-
-        if (!userFounded) {
-            return null;
-        }
-
-        const userUpdated = await this.clientService.updateById(clientId, input);
-
-        if (!userUpdated) {
-            return null;
-        }
-
-        return userUpdated;
+    if (!userFounded) {
+      return null;
     }
+
+    const userUpdated = await this.clientService.updateById(clientId, input);
+
+    if (!userUpdated) {
+      return null;
+    }
+
+    return userUpdated;
+  }
 }
