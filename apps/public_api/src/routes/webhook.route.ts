ddc6942c17f26cbc5831cc4f4b5d824680aba208
victor-pagnozi <victor.pagnozi@gmail.com>
2024-04-25 15:49:59 -0300

    made endpoint to test webhook of the zoop

---
+++ apps/public_api/src/routes/webhook.route.ts
@@ -0,0 +1,11 @@
+import WebhookController from '@/controllers/webhook';
+import { FastifyInstance } from 'fastify';
+import { container } from 'tsyringe';
+
+export default async function webhooksRoutes(server: FastifyInstance) {
+  const controller = container.resolve(WebhookController);
+
+  server.post('/webhook/payments', {
+    handler: controller.paymentWebhook,
+  });
+}
