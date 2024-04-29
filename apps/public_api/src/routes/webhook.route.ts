e4a84a97673b1bf5fdd6c69a09c67941df876ac0
victor-pagnozi <victor.pagnozi@gmail.com>
2024-04-29 17:39:43 -0300

    removed websocket service not used

--- apps/public_api/src/routes/webhook.route.ts
+++ apps/public_api/src/routes/webhook.route.ts
@@ -1,6 +1,5 @@
 import WebhookController from '@/controllers/webhook';
 import { FastifyInstance } from 'fastify';
-import WebSocket from 'ws';
 import { container } from 'tsyringe';
 
 export default async function webhooksRoutes(app: FastifyInstance) {
@@ -8,25 +7,5 @@ export default async function webhooksRoutes(app: FastifyInstance) {
 
   app.post('/webhook/payments', {
     handler: controller.paymentWebhook,
-    websocket: true,
-  });
-
-  const wsServer = new WebSocket.Server({ noServer: true });
-
-  wsServer.on('connection', (ws) => {
-    ws.on('message', (message: string) => {
-      console.log('Mensagem recebida:', message);
-      ws.send('Olá cliente!');
-    });
-
-    ws.on('close', () => {
-      console.log('Conexão fechada.');
-    });
-  });
-
-  app.server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, (ws) => {
-      wsServer.emit('connection', ws, request);
-    });
   });
 }
