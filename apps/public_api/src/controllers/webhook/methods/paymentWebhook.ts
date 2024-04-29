249fcf006b0ae2d851c8c05a637057028ae6a051
victor-pagnozi <victor.pagnozi@gmail.com>
2024-04-29 15:33:23 -0300

    refact: testing new structure to create websocket on fastify

--- apps/public_api/src/controllers/webhook/methods/paymentWebhook.ts
+++ apps/public_api/src/controllers/webhook/methods/paymentWebhook.ts
@@ -1,16 +1,22 @@
 import { HTTPStatusCode } from '@core/common/enums/HTTPStatusCode';
 import { sendResponse } from '@core/common/functions/sendResponse';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { WebSocket } from 'ws';
 
 export const paymentWebhook = async (
-  request: FastifyRequest,
-  reply: FastifyReply
+  socket: WebSocket,
+  request: FastifyRequest
 ) => {
   console.log(request.body);
   console.log(request.params);
   console.log(request.query);
 
-  return sendResponse(reply, {
-    httpStatusCode: HTTPStatusCode.OK,
+  socket.on('message', (message) => {
+    // message.toString() === 'hi from client'
+    socket.send('hi from wildcard route');
   });
+
+  // return sendResponse(reply, {
+  //   httpStatusCode: HTTPStatusCode.OK,
+  // });
 };
--- apps/public_api/src/routes/webhook.route.ts
+++ apps/public_api/src/routes/webhook.route.ts
@@ -6,27 +6,34 @@ import { container } from 'tsyringe';
 export default async function webhooksRoutes(app: FastifyInstance) {
   const controller = container.resolve(WebhookController);
 
-  app.post('/webhook/payments', {
-    handler: controller.paymentWebhook,
-    websocket: true,
-  });
+  app.get(
+    '/webhook/payments',
+    {
+      websocket: true,
+    },
+    (socket, request) => {
+      controller.paymentWebhook(socket, request);
+    }
+  );
 
-  const wsServer = new WebSocket.Server({ noServer: true });
+  // const wsServer = new WebSocket.Server({ noServer: true });
 
-  wsServer.on('connection', (ws) => {
-    ws.on('message', (message: string) => {
-      console.log('Mensagem recebida:', message);
-      ws.send('Olá cliente!');
-    });
+  // wsServer.on('connection', (ws, err) => {
+  //   ws.on('message', (message: string) => {
+  //     console.log('Mensagem recebida:', message);
+  //     ws.send('Olá cliente!');
+  //   });
 
-    ws.on('close', () => {
-      console.log('Conexão fechada.');
-    });
-  });
+  //   ws.on('close', () => {
+  //     console.log('errrrrrrrrrrrrrrrrrrrrrrr');
+  //     console.log(err);
+  //     console.log('Conexão fechada.');
+  //   });
+  // });
 
-  app.server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, (ws) => {
-      wsServer.emit('connection', ws, request);
-    });
-  });
+  // app.server.on('upgrade', (request, socket, head) => {
+  //   wsServer.handleUpgrade(request, socket, head, (ws) => {
+  //     wsServer.emit('connection');
+  //   });
+  // });
 }
