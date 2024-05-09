e7cd9678114290865f4e8ef3187437c03aae1e87
Maycon Douglas <contatomaycon@gmail.com>
2024-05-09 14:16:29 -0300

    test

--- .github/scripts/check-duplicates.js
+++ .github/scripts/check-duplicates.js
@@ -1,14 +1,8 @@
 const fs = require("fs");
 const dupKeyValidator = require("json-dup-key-validator");
 
-const jsonContentPt = fs.readFileSync(
-  "packages/core/plugins/i18next/locales/pt/translation.json",
-  "utf8"
-);
-const jsonContentEn = fs.readFileSync(
-  "packages/core/plugins/i18next/locales/en/translation.json",
-  "utf8"
-);
+const jsonContentPt = fs.readFileSync("pt/translation.json", "utf8");
+const jsonContentEn = fs.readFileSync("en/translation.json", "utf8");
 
 try {
   dupKeyValidator.parse(jsonContentPt, true);
