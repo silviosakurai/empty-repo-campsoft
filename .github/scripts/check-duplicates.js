e342567ed372b7f2a69da8e9db0de465f5f9fe13
Maycon Douglas <contatomaycon@gmail.com>
2024-05-09 12:54:12 -0300

    test

--- .github/scripts/check-duplicates.js
+++ .github/scripts/check-duplicates.js
@@ -1,19 +1,24 @@
 const fs = require("fs");
 
 function checkForDuplicateKeys(jsonObject, filePath) {
-  const keys = new Set();
   function checkDuplicates(obj, path = []) {
+    const keys = new Set();
+
     Object.keys(obj).forEach((key) => {
-      if (typeof obj[key] === "object" && obj[key] !== null) {
-        return checkDuplicates(obj[key], path.concat(key));
-      }
       const fullPath = path.concat(key).join(".");
-      if (keys.has(fullPath)) {
+      if (keys.has(key)) {
         throw new Error(
-          `Duplicate key "${fullPath}" found in file ${filePath}`
+          `Duplicate key "${key}" found at path "${fullPath}" in file ${filePath}`
         );
       }
-      keys.add(fullPath);
+      keys.add(key);
+      if (
+        typeof obj[key] === "object" &&
+        obj[key] !== null &&
+        !Array.isArray(obj[key])
+      ) {
+        checkDuplicates(obj[key], path.concat(key));
+      }
     });
   }
   checkDuplicates(jsonObject);
@@ -26,7 +31,7 @@ process.argv.slice(2).forEach((filePath) => {
     checkForDuplicateKeys(json, filePath);
     console.log(`No duplicate keys found in ${filePath}`);
   } catch (e) {
-    console.error(`JSON parsing error in ${filePath}: ${e.message}`);
+    console.error(`Error in ${filePath}: ${e.message}`);
     process.exit(1);
   }
 });
