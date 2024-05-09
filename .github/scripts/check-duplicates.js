82cd2503d5b19eadcccd22cc7f5ecba4ff93aa47
Maycon Douglas <contatomaycon@gmail.com>
2024-05-09 14:49:21 -0300

    test

--- .github/scripts/check-duplicates.js
+++ .github/scripts/check-duplicates.js
@@ -1,20 +1,30 @@
 const fs = require("fs");
-const dupKeyValidator = require("json-dup-key-validator");
 
-const jsonContentPt = fs.readFileSync(
-  "../../packages/core/plugins/i18next/locales/pt/translation.json",
-  "utf8"
+const jsonData = JSON.parse(
+  fs.readFileSync(
+    "packages/core/plugins/i18next/locales/en/translation.json",
+    "utf8"
+  )
 );
-const jsonContentEn = fs.readFileSync(
-  "../../packages/core/plugins/i18next/locales/en/translation.json",
-  "utf8"
-);
+const keysSeen = new Set();
 
-try {
-  dupKeyValidator.parse(jsonContentPt, true);
-  dupKeyValidator.parse(jsonContentEn, true);
+let hasDuplicates = false;
 
+function checkKeys(object) {
+  for (const key of Object.keys(object)) {
+    if (keysSeen.has(key)) {
+      console.error(`Duplicate key found: ${key}`);
+      hasDuplicates = true;
+    }
+    keysSeen.add(key);
+    if (typeof object[key] === "object" && object[key] !== null) {
+      checkKeys(object[key]);
+    }
+  }
+}
+
+checkKeys(jsonData);
+
+if (!hasDuplicates) {
   console.log("No duplicate keys found.");
-} catch (e) {
-  console.error("Duplicate keys detected:", e.message);
 }
