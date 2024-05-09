dd47f72b12a876024d67553fa87797dbbd7b9975
Vinicius Menezes <viny2999@gmail.com>
2024-05-09 10:30:42 -0300

    fix identation

--- packages/core/schema/partner/partnerCreateBodySchema.ts
+++ packages/core/schema/partner/partnerCreateBodySchema.ts
@@ -3,13 +3,13 @@ import { PartnerStatus } from "@core/common/enums/models/partner";
 
 export const partnerCreateBodySchema = Type.Object({
   id_company_type: Type.Number(),
-	status: Type.String({ enum: Object.values(PartnerStatus) }),
-	trade_name: Type.String(),
-	legal_name: Type.String(),
-	responsible_name: Type.String(),
-	responsible_lastname: Type.String(),
-	responsible_email: Type.String(),
-	phone: Type.Number(),
-	cnpj: Type.Number(),
-	obs: Type.String(),
+  status: Type.String({ enum: Object.values(PartnerStatus) }),
+  trade_name: Type.String(),
+  legal_name: Type.String(),
+  responsible_name: Type.String(),
+  responsible_lastname: Type.String(),
+  responsible_email: Type.String(),
+  phone: Type.Number(),
+  cnpj: Type.Number(),
+  obs: Type.String(),
 });
